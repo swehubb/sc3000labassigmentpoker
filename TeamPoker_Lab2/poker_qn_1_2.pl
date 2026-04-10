4ab6811109a73377a96a8ce9868c9a33d2b98f90
Shuma <shumaterashima@gmail.com>
2026-04-10 14:07:35 +0800

    Add files via upload

---
+++ TeamPoker_Lab2/poker_qn_1_2.pl
@@ -0,0 +1,24 @@
+% =========================================
+% Exercise 1: The Smart Phone Rivalry
+% =========================================
+
+% Facts
+competitor(sumsum, appy).
+developed(sumsum, galactica_s3).
+stole(stevey, galactica_s3).
+boss(stevey, appy).
+smart_phone_technology(galactica_s3).
+
+% Rules
+rival(X, Y) :-
+    competitor(X, Y).
+
+business(X) :-
+    smart_phone_technology(X).
+
+unethical(X) :-
+    boss(X, Y),
+    rival(W, Y),
+    developed(W, Z),
+    stole(X, Z),
+    business(Z).
